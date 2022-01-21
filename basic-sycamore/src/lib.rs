1f8012425130ff5fea61c1a55ced642ce631b57c
Tejas Sanap <sanap.tejas@gmail.com>
2022-01-21 17:46:34 +0530

    Add dropdown and radiobutton example for `basic-sycamore` crate.

--- basic-sycamore/src/lib.rs
+++ basic-sycamore/src/lib.rs
@@ -1,19 +1,106 @@
 use sycamore::{
     builder::html::*,
-    prelude::{component, View},
-    view,
+    prelude::{component, Signal, View},
 };
 use wasm_bindgen::prelude::wasm_bindgen;
 
-#[component(MyPage<G>)]
-fn my_page() -> View<G> {
+#[wasm_bindgen]
+extern "C" {
+    // Use `js_namespace` here to bind `console.log(..)` instead of just
+    // `log(..)`
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+#[derive(Clone)]
+struct FieldInfo {
+    title: String,
+    value: Signal<String>,
+    // Vec<(value, title)>
+    options: Option<Vec<(String, String)>>,
+}
+
+#[component(MyRadiobutton<G>)]
+fn render(info: FieldInfo) -> View<G> {
+    let radiobutton_value = info.value.clone();
+
     div()
-        .child(h1().text("Hello, world!").build())
-        .child(h2().text("Bye, world!").build())
+        .child(h2().text(info.title.clone()).build())
+        .child(
+            form()
+                .child(View::new_fragment(
+                    info.options
+                        .unwrap()
+                        .iter()
+                        .map(|(value, title)| {
+                            div()
+                                .child(
+                                    input()
+                                        .id(value)
+                                        .attr("type", "radio")
+                                        .attr("value", value)
+                                        .attr("name", info.title.clone())
+                                        .build(),
+                                )
+                                .child(label().attr("for", value).text(title).build())
+                                .build()
+                        })
+                        .collect(),
+                ))
+                .bind_value(radiobutton_value.clone())
+                .on("change", move |_| {
+                    log((*radiobutton_value.get()).to_string().as_str())
+                })
+                .build(),
+        )
+        .build()
+}
+
+#[component(MyDropdown<G>)]
+fn render(info: FieldInfo) -> View<G> {
+    let dropdown_value = info.value.clone();
+    div()
+        .child(h2().text(info.title).build())
+        .child(
+            select()
+                .child(View::new_fragment(
+                    info.options
+                        .unwrap()
+                        .iter()
+                        .map(|(value, title)| option().attr("value", value).text(title).build())
+                        .collect(),
+                ))
+                .bind_value(info.value)
+                .on("change", move |_| {
+                    log((*dropdown_value.get()).to_string().as_str())
+                })
+                .build(),
+        )
         .build()
 }
 
 #[wasm_bindgen(start)]
 pub fn run_app() {
-    sycamore::render(|| view! { MyPage() });
+    let info = FieldInfo {
+        title: "Some dropdown".to_string(),
+        value: Signal::new(String::new()),
+        options: Some(vec![
+            ("1".to_string(), "apples".to_string()),
+            ("2".to_string(), "oranges".to_string()),
+            ("3".to_string(), "peaches".to_string()),
+            ("4".to_string(), "pinepapples".to_string()),
+        ]),
+    };
+    sycamore::render(|| component::<_, MyDropdown<_>>(info));
+
+    let info = FieldInfo {
+        title: "Some Radio buttons".to_string(),
+        value: Signal::new(String::new()),
+        options: Some(vec![
+            ("1".to_string(), "apples".to_string()),
+            ("2".to_string(), "oranges".to_string()),
+            ("3".to_string(), "peaches".to_string()),
+            ("4".to_string(), "pinepapples".to_string()),
+        ]),
+    };
+    sycamore::render(|| component::<_, MyRadiobutton<_>>(info));
 }
