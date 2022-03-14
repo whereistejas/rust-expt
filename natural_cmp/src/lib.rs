9071c9d0fd25c8ad0a212503b3441fa2f29f4594
Tejas Sanap <sanap.tejas@gmail.com>
2022-03-14 11:10:51 +0000

    Comment out current implementation for `natural_cmp`.
    Signed-off-by: Tejas Sanap <sanap.tejas@gmail.com>

--- natural_cmp/src/lib.rs
+++ natural_cmp/src/lib.rs
@@ -1,88 +1,104 @@
 use std::cmp::Ordering;
 
-pub fn str_to_pieces(value: &str) -> Vec<Pieces> {
-    let value = value.as_bytes();
+// pub fn natual_cmp(a: &str, b: &str) -> Ordering {
+//     let pieces_a = str_to_pieces(a);
+//     let pieces_b = str_to_pieces(b);
 
-    let mut pieces = Vec::new();
-    let mut start: usize = 0;
-    let mut end: usize;
+//     for (a, b) in pieces_a.iter().zip(pieces_b.iter()) {
+//         return a.partial_cmp(b).unwrap();
+//     }
+// }
 
-    let mut piece_type = 0;
+// pub fn str_to_pieces(value: &str) -> Vec<Pieces> {
+//     let value = value.as_bytes();
 
-    for (idx, c) in value.iter().enumerate() {
-        let prev_type = piece_type;
+//     let mut pieces = Vec::new();
+//     let mut start: usize = 0;
+//     let mut end: usize;
 
-        if c.is_ascii_digit() {
-            // the number is a plain digit
-            piece_type = 1;
-        } else if c == &b'.' {
-            // looks like we have run into a decimal point
-            // check if the character before and after the decimal point is a digit
-            if value[idx - 1].is_ascii_digit() && value[idx + 1].is_ascii_digit() {
-                piece_type = 1;
-            } else {
-                piece_type = 2;
-            }
-        } else {
-            // its a plain character
-            piece_type = 2;
-        }
+//     let mut piece_type = 0;
 
-        // the first and last character of the string are special cases
-        if (piece_type != prev_type && prev_type != 0) || (idx + 1) == value.len() {
-            end = if (idx + 1) == value.len() {
-                idx + 1
-            } else {
-                idx
-            };
+//     for (idx, c) in value.iter().enumerate() {
+//         let prev_type = piece_type;
 
-            if prev_type == 1 {
-                let temp = &value[start..end];
-                pieces.push(Pieces::Digits(
-                    std::str::from_utf8(temp).unwrap().parse::<f64>().unwrap(),
-                ))
-            }
+//         // check what kind of piece it is.
+//         // it can either be:
+//         // 1 - Digits
+//         // 2 - Chars
+//         if c.is_ascii_digit() {
+//             // the number is a plain digit
+//             piece_type = 1;
+//         } else if c == &b'.' {
+//             // looks like we have run into a decimal point
+//             // check if the character before and after the decimal point is a digit
+//             if value[idx - 1].is_ascii_digit() && value[idx + 1].is_ascii_digit() {
+//                 piece_type = 1;
+//             } else {
+//                 piece_type = 2;
+//             }
+//         } else {
+//             // its a plain character
+//             piece_type = 2;
+//         }
 
-            if prev_type == 2 {
-                let temp = &value[start..end];
-                pieces.push(Pieces::Chars(std::str::from_utf8(temp).unwrap()))
-            }
+//         // the first and last character of the string are special cases
+//         if (piece_type != prev_type && prev_type != 0) || (idx + 1) == value.len() {
+//             // handle the special case of being at the end of the string
+//             end = if (idx + 1) == value.len() {
+//                 idx + 1
+//             } else {
+//                 idx
+//             };
 
-            start = idx;
-        }
-    }
+//             if prev_type == 1 {
+//                 let temp = &value[start..end];
+//                 pieces.push(Pieces::Digits(
+//                     std::str::from_utf8(temp).unwrap().parse::<f64>().unwrap(),
+//                 ))
+//             }
 
-    pieces
-}
+//             if prev_type == 2 {
+//                 let temp = &value[start..end];
+//                 pieces.push(Pieces::Chars(std::str::from_utf8(temp).unwrap()))
+//             }
 
-// The idea is to force each parse into a standard type, so that we can leave the comparison to the std library.
-#[derive(Debug, PartialEq)]
-pub enum Pieces<'a> {
-    Chars(&'a str),
-    Digits(f64),
-}
+//             // set the next piece's start value to the end of the current piece.
+//             start = idx;
+//         }
+//     }
 
-impl<'a> PartialOrd for Pieces<'a> {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        fn compare<T>(a: &T, b: &T) -> Option<Ordering> {
-            if me.gt(other) {
-                Some(Ordering::Greater)
-            } else if me.le(other) {
-                Some(Ordering::Less)
-            } else {
-                Some(Ordering::Equal)
-            }
-        }
-        match (self, other) {
-            (Pieces::Chars(me), Pieces::Chars(other)) => compare(me, other),
-            (Pieces::Digits(me), Pieces::Digits(other)) => compare(me, other),
-            // numbers always come before alphabets
-            (Pieces::Chars(_), Pieces::Digits(_)) => Some(Ordering::Greater),
-            // alphabets always come after numbers
-            (Pieces::Digits(_), Pieces::Chars(_)) => Some(Ordering::Less),
-        }
-    }
-}
+//     pieces
+// }
+
+// // The idea is to force each parse into a standard type, so that we can leave the comparison to the std library.
+// #[derive(Debug, PartialEq)]
+// pub enum Pieces<'a> {
+//     Chars(&'a str),
+//     Digits(f64),
+// }
+
+// impl<'a> PartialOrd for Pieces<'a> {
+//     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+//         fn compare<T: PartialOrd>(a: &T, b: &T) -> Option<Ordering> {
+//             if a.gt(b) {
+//                 Some(Ordering::Greater)
+//             } else if a.lt(b) {
+//                 Some(Ordering::Less)
+//             } else {
+//                 Some(Ordering::Equal)
+//             }
+//         }
+
+//         match (self, other) {
+//             (Pieces::Chars(me), Pieces::Chars(other)) => compare(me, other),
+//             (Pieces::Digits(me), Pieces::Digits(other)) => compare(me, other),
+//             // numbers always come before alphabets
+//             (Pieces::Chars(_), Pieces::Digits(_)) => Some(Ordering::Greater),
+//             // alphabets always come after numbers
+//             (Pieces::Digits(_), Pieces::Chars(_)) => Some(Ordering::Less),
+//         }
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
