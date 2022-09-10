16485bc22b3b019ca78d399cff439a0be4fa35e3
Tejas Sanap <sanap.tejas@gmail.com>
2022-09-10 18:24:08 +0100

    start a docker container and stdout

--- bollarddss/src/main.rs
+++ bollarddss/src/main.rs
@@ -1,21 +1,62 @@
-use bollard::image::ListImagesOptions;
+use bollard::container::{
+    AttachContainerOptions, AttachContainerResults, Config, CreateContainerOptions, LogOutput,
+    RemoveContainerOptions, StartContainerOptions,
+};
 use bollard::Docker;
 
+use futures::StreamExt;
 use std::default::Default;
 
 #[tokio::main]
 async fn main() {
     let docker = Docker::connect_with_local_defaults().unwrap();
 
-    let images = &docker
-        .list_images(Some(ListImagesOptions::<String> {
-            all: true,
-            ..Default::default()
-        }))
+    let config = Config {
+        cmd: Some(vec!["spec"]),
+        image: Some("airbyte/source-amazon-ads:0.1.19"),
+        ..Default::default()
+    };
+
+    let create_options = Some(CreateContainerOptions {
+        name: "some-container",
+    });
+
+    let _response = docker
+        .create_container(create_options, config)
         .await
         .unwrap();
 
-    for image in images {
-        println!("{image:?}")
+    let attach_options = Some(AttachContainerOptions::<String> {
+        stdin: Some(true),
+        stdout: Some(true),
+        stderr: Some(true),
+        stream: Some(true),
+        logs: Some(true),
+        ..Default::default()
+    });
+
+    let AttachContainerResults { mut output, input } = docker
+        .attach_container("some-container", attach_options)
+        .await
+        .unwrap();
+
+    docker
+        .start_container("some-container", None::<StartContainerOptions<String>>)
+        .await
+        .unwrap();
+
+    for line in output.next().await {
+        match line.unwrap() {
+            LogOutput::StdOut { message }
+            | LogOutput::StdErr { message }
+            | LogOutput::StdIn { message }
+            | LogOutput::Console { message } => {
+                println!("{}", std::str::from_utf8(&message).unwrap())
+            }
+        }
     }
+
+    let _ = docker
+        .remove_container("some-container", None::<RemoveContainerOptions>)
+        .await;
 }
