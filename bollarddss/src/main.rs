dd657985842faa0560038808d3ac4fc99696d6d5
Tejas Sanap <sanap.tejas@gmail.com>
2022-09-28 17:55:31 +0100

    Add filters to find specific images.

--- bollarddss/src/main.rs
+++ bollarddss/src/main.rs
@@ -2,14 +2,30 @@ use bollard::container::{
     AttachContainerOptions, AttachContainerResults, Config, CreateContainerOptions, LogOutput,
     RemoveContainerOptions, StartContainerOptions,
 };
+use bollard::image::{CreateImageOptions, ListImagesOptions};
 use bollard::Docker;
 
 use futures::StreamExt;
+use std::collections::HashMap;
 use std::default::Default;
 
 #[tokio::main]
 async fn main() {
     let docker = Docker::connect_with_local_defaults().unwrap();
+    let mut images = Vec::new();
+
+    let pull_options = Some(CreateImageOptions {
+        from_image: "airbyte/source-amazon-ads:0.1.19",
+        ..Default::default()
+    });
+    let mut response = docker.create_image(pull_options, None, None);
+
+    for result in response.next().await {
+        match result {
+            Ok(_) => break,
+            Err(_) => continue,
+        }
+    }
 
     let config = Config {
         cmd: Some(vec!["spec"]),
@@ -20,6 +36,27 @@ async fn main() {
     let create_options = Some(CreateContainerOptions {
         name: "some-container",
     });
+
+    let mut filters = HashMap::new();
+    filters.insert("reference", vec!["airbyte/source-amazon-ads:0.1.19"]);
+
+    images = docker
+        .list_images(Some(ListImagesOptions {
+            filters: filters.clone(),
+            ..Default::default()
+        }))
+        .await
+        .unwrap();
+
+    while images.is_empty() {
+        images = docker
+            .list_images(Some(ListImagesOptions {
+                filters: filters.clone(),
+                ..Default::default()
+            }))
+            .await
+            .unwrap();
+    }
 
     let _response = docker
         .create_container(create_options, config)
@@ -56,7 +93,11 @@ async fn main() {
         }
     }
 
+    let remove_options = Some(RemoveContainerOptions {
+        force: true,
+        ..Default::default()
+    });
     let _ = docker
-        .remove_container("some-container", None::<RemoveContainerOptions>)
+        .remove_container("some-container", remove_options)
         .await;
 }
