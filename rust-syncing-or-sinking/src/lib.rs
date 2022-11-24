7cf40c90a42a7263fcef1563dc820c2e8f38d07b
Tejas Sanap <email@whereistejas.xyz>
2022-11-24 13:56:56 +0000

    try wrapping the rc in an arc.

--- rust-syncing-or-sinking/src/lib.rs
+++ rust-syncing-or-sinking/src/lib.rs
@@ -1,17 +1,21 @@
 #[cfg(test)]
 mod tests {
     use std::rc::Rc;
-    use std::sync::mpsc;
+    use std::sync::{mpsc, Arc};
     use std::thread;
 
     #[test]
     fn it_works() {
         let (tx, rx) = mpsc::channel::<u32>();
-        let value: Rc<u32> = Rc::new(12);
+        let value_: Rc<u32> = Rc::new(12);
+
+        // Even if you wrap an `Rc` in a `Arc`, you still cannot move it around.
+        let value: Arc<Rc<u32>> = Arc::new(value_);
 
         let _ = thread::spawn(|| {
-            let value: u32 = unsafe { *Rc::<u32>::as_ptr(&value) };
-            tx.send(value).unwrap()
+            let value = unsafe { *Arc::<Rc<u32>>::as_ptr(&value) };
+            let inner: u32 = unsafe { *Rc::<u32>::as_ptr(&value) };
+            tx.send(inner).unwrap()
         });
 
         assert_eq!(rx.recv(), Ok(12));
